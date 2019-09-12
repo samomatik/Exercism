693d1581c479520429781e68fab91f336b664de2
Samitha Padukkage Don <samitha.312@gmail.com>
2019-09-12 11:42:47 +1000

    protein-translation complete and submitted

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -3,6 +3,36 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+const PROTEINS = {
+  'AUG': 'Methionine',
+  'UUU': 'Phenylalanine',
+  'UUC': 'Phenylalanine',
+  'UUA': 'Leucine',
+  'UUG': 'Leucine',
+  'UCU': 'Serine',
+  'UCC': 'Serine',
+  'UCA': 'Serine',
+  'UCG': 'Serine',
+  'UAU': 'Tyrosine',
+  'UAC': 'Tyrosine',
+  'UGU': 'Cysteine',
+  'UGC': 'Cysteine',
+  'UGG': 'Tryptophan',
+  'UAA': 'STOP',
+  'UAG': 'STOP',
+  'UGA': 'STOP'
+}
+
+export const translate = (rna) => {
+  if (!rna) return [];
+  let split = rna.match(/.{1,3}/g);
+  let proteins = [];
+  
+  for (var i = 0; i < split.length; i++) {
+    if (PROTEINS[split[i]] === 'STOP') return proteins;
+    if (!PROTEINS[split[i]]) throw 'Invalid codon';
+    proteins.push(PROTEINS[split[i]]);
+  }
+
+  return proteins;
 };
