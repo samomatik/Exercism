9e74bc6cd66b5bd6407ad35b4f9aa85a1c16527a
Samitha Padukkage Don <samitha.312@gmail.com>
2019-09-13 11:03:46 +1000

    Exercism complete

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const value = () => {
-  throw new Error("Remove this statement and implement this function");
+const VALS = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+}
+
+export const value = (colors) => {
+  let num = '' + VALS[colors[0]] + VALS[colors[1]];
+  return parseInt(num);
 };
