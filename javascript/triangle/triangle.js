7706a2d85a586b702a975c3c3bd2364e6920953e
Samitha Padukkage Don <sam@samomtik.com>
2019-09-16 13:23:10 +1000

    triangle exercise complete

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -4,11 +4,25 @@
 //
 
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(s1, s2, s3) {
+    this.s1 = s1
+    this.s2 = s2
+    this.s3 = s3
   }
 
   kind() {
-    throw new Error("Remove this statement and implement this function");
+    if (this.s1 <= 0 || this.s2 <= 0 || this.s3 <= 0) {
+      throw new Error("Not a triangle!");
+    } else if (this.s1 + this.s2 <= this.s3 || this.s1 + this.s3 <= this.s2 || this.s2 + this.s3 <= this.s1) {
+      throw new Error("Not a triangle!");
+    } else if (this.s1 === this.s2 && this.s2 === this.s3) {
+      return "equilateral"
+    } else if (this.s1 !== this.s2 && this.s2 !== this.s3 && this.s1 !== this.s3) {
+      return "scalene"
+    } else if (this.s1 === this.s2 || this.s2 === this.s3 || this.s1 === this.s3) {
+      return "isosceles"
+    }else {
+      return "what"
+    }
   }
 }
