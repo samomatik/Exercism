8b964d6b0cd99666a16c78af747746920edf9b53
Samitha Padukkage Don <samitha.312@gmail.com>
2019-09-18 19:33:53 +1000

    collatz-conjecture complete

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (n) => {
+  if (n <= 0) {
+    throw new Error("Only positive numbers are allowed");
+  } else if (n === 1) {
+    return 0;
+  } else {
+    let counter = 0;
+    while (n !== 1) {
+      n % 2 == 0 ? n = n / 2 : n = (3 * n) + 1;
+      counter = counter + 1;
+    }
+    return counter;
+  }
 };
